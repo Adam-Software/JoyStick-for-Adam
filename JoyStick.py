54f31077c86c75224e5f0543303ca97ae8e31b0c
ADAM <36617611+ALGN86@users.noreply.github.com>
2023-08-21 20:50:19 +1000

    Update JoyStick.py

--- JoyStick.py
+++ JoyStick.py
@@ -3,24 +3,29 @@ import numpy as np
 from adam_sdk import AdamManager
 from adam_sdk import SerializableCommands
 from adam_sdk import MotorCommand
+import time
 
 adamController = None
+
+
 adamController = AdamManager()
 
 pygame.init()
 #initialise the joystick module
 pygame.joystick.init()
 
-#define max angle in percent
-MAX_X_ANGLE = 100
-MAX_Y_ANGLE = 100
+#define screen size
+SCREEN_WIDTH = 100
+SCREEN_HEIGHT = 100
+
+
 
 #create empty list to store joysticks
 joysticks = []
 
-#set default position
-x = MAX_X_ANGLE / 2
-y = MAX_Y_ANGLE / 2
+#create player rectangle
+x = SCREEN_WIDTH / 2
+y = SCREEN_HEIGHT / 2
 
 x1 = x
 y1 = y
@@ -37,9 +42,19 @@ y6 = y
 x7 = x
 y7 = y
 
+
+
 #game loop
 run = True
 while run:
+
+
+  #show number of connected joysticks
+  #draw_text("Controllers: " + str(pygame.joystick.get_count()), font, pygame.Color("azure"), 10, 10)
+  #for joystick in joysticks:
+  #  draw_text("Battery Level: " + str(joystick.get_power_level()), font, pygame.Color("azure"), 10, 35)
+  #  draw_text("Controller Type: " + str(joystick.get_name()), font, pygame.Color("azure"), 10, 60)
+  #  draw_text("Number of axes: " + str(joystick.get_numaxes()), font, pygame.Color("azure"), 10, 85)
 
   for joystick in joysticks:
 
@@ -55,13 +70,13 @@ while run:
     def CalcMovment(x, y, H, W):
 
       if abs(H) > 0.01:
-         x += H * 0.5
+         x -= H * 0.5
          if x > 100:
             x = 100
          if x < 0:
             x = 0
       if abs(W) > 0.01:
-         y += W * 0.5
+         y -= W * 0.5
          if y > 100:
             y = 100
          if y < 0:
@@ -91,7 +106,7 @@ while run:
       if x < 0:
          x = 0
 
-      y -= input[1] * 0.5
+      y += input[1] * 0.5
       if y > 100:
          y = 100
       if y < 0:
@@ -128,7 +143,7 @@ while run:
 
     def LeftArmFunc(x2, y2):
       # Left Bumper
-      if joystick.get_button(4):
+      if not joystick.get_button(4):
         xy = AnalogueSticks([0, 1])
         xy = CalcMovment(x2, y2, xy[0], xy[1])
         return (xy)
@@ -137,10 +152,10 @@ while run:
 
     def RightArmFunc(x3, y3):
       # Right Bumper
-      if joystick.get_button(5):
+      if not joystick.get_button(5):
         xy = AnalogueSticks([3, 4])
         #print(xy)
-        xy = CalcMovment(x3, y3, xy[0], xy[1])
+        xy = CalcMovment(x3, y3, -1 * xy[0], xy[1])
         return (xy)
       else:
         return (x3, y3)
@@ -151,28 +166,28 @@ while run:
       return (xy)
 
     def MecanumWheelsMoveFunc(x6, y6):
-      #Left Bumper
-      if not joystick.get_button(4):
+      # Left Bumper
+      if joystick.get_button(4):
         xy = AnalogueSticks([0, 1])
-        xy = CalcVectorMovment(x6, y6, xy[0], xy[1])
+        xy = CalcVectorMovment(x6, y6, -1 * xy[0], -1 * xy[1])
         return (xy)
       else:
-        xy = AnalogueSticks([0, 1])
-        return (x6, y6, xy)
+        xy = [0,0,0,0]
+        return (xy)
 
     def MecanumWheelsRotateFunc(x7, y7):
-      #Right Bumper
-      if not joystick.get_button(5):
+      # Right Bumper
+      if joystick.get_button(5):
         xy = AnalogueSticks([2, 3])
-        xy = CalcVectorMovment(x7, y7, xy[0], xy[1])
+        xy = CalcVectorMovment(x7, y7, -1 * xy[0], -1 * xy[1])
         return (xy)
       else:
-        xy = AnalogueSticks([2, 3])
-        return (x7, y7, xy)
+        xy = [0, 0, 0, 0]
+        return (xy)
 
-    #HomePos
-    if joystick.get_button(10):
-      adamController.return_to_start_position()
+    # HomePos
+    if joystick.get_button(8):
+        adamController.return_to_start_position()
 
     #main
     xy = NeckHeadFunc(x, y)
@@ -200,25 +215,30 @@ while run:
     x4 = xy[0]
     y4 = xy[1]
 
-    xy = MecanumWheelsMoveFunc(x6, y6)
+
+    xym = MecanumWheelsMoveFunc(x6, y6)
+    xyr = MecanumWheelsRotateFunc(x7, y7)
     #print("MecanumWheelsMove:", xy)
-    x6 = xy[0]
-    y6 = xy[1]
-
-    xy = MecanumWheelsRotateFunc(x7, y7)
+    x6 = xym[2]
+    y6 = xym[3]
     #print("MecanumWheelsRotate:", xy)
-    x7 = xy[0]
-    y7 = xy[1]
+    x7 = xyr[2]
+    y7 = xyr[3]
 
-    adamController.move((x6, y6), x7)
+    if joystick.get_button(4) or joystick.get_button(5):
+        adamController.move((x6, y6), y7)
+
     adamController.handle_command(commands=SerializableCommands([MotorCommand('neck', x), MotorCommand('head', y),
                                                                  MotorCommand('chest', x1), MotorCommand('press', y1),
                                                                  MotorCommand('left_hand', x4), MotorCommand('right_hand', y4),
                                                                  MotorCommand('left_upper_arm', x2), MotorCommand('right_upper_arm', x3),
                                                                  MotorCommand('left_shoulder', y2), MotorCommand('right_shoulder', y3)]))
 
+
       #event handler
   for event in pygame.event.get():
     if event.type == pygame.JOYDEVICEADDED:
       joy = pygame.joystick.Joystick(event.device_index)
       joysticks.append(joy)
+
+
